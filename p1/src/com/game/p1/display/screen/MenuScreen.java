5ea30a799411f2cd13e036d45e94e1fcec57a8c7
virgilio catubig <catubig37@yahoo.com>
2013-06-09 19:41:05 -0700

    fuck commit

---
+++ p1/src/com/game/p1/display/screen/MenuScreen.java
@@ -0,0 +1,13 @@
+package com.game.p1.display.screen;
+
+import com.game.framework.display.DisplayScreen;
+import com.game.p1.utils.Config;
+
+public class MenuScreen extends DisplayScreen{
+
+	public MenuScreen() {
+		super(Config.SCREEN_WIDTH,Config.SCREEN_HEIGHT );
+		// TODO Auto-generated constructor stub
+	}
+
+}
